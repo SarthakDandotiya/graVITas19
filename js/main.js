7f1f8ed958fdf4a83b77bdff5e9b2939bb1e5bd0
Rajvardhan99 <rajvardhan1999@gmail.com>
2019-05-08 20:16:39 +0530

    gallery mobileview

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
 // NAVBAR
 $(document).ready(function() {
-	$('.sidenav').sidenav();
+  $(".sidenav").sidenav();
 });
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-	var currentScrollPos = window.pageYOffset;
-	if (prevScrollpos > currentScrollPos) {
-		document.getElementById('navbar').style.top = '0';
-	} else {
-		document.getElementById('navbar').style.top = '-100px';
-	}
-	prevScrollpos = currentScrollPos;
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
 };
 
 //SCROLL
 $(document).ready(function() {
-	$('a.scroll').click(function(event) {
-		event.preventDefault();
-		$('html, body').animate(
-			{ scrollTop: $($(this).attr('href')).offset().top },
-			500
-		);
-	});
+  $("a.scroll").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500
+    );
+  });
 });
 
 // GALLERY
-$('.img-slide').slick({
-	lazyLoad: 'progressive',
-	autoplay: true,
-	autoplaySpeed: 2000,
-	slidesToShow: 3,
-	arrows: false,
-	infinte: true,
-	swipeToSlide: true
+console.log(window.innerWidth);
+$(".img-slide").slick({
+  lazyLoad: "progressive",
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: window.innerWidth < 650 ? (window.innerWidth < 450 ? 1 : 2) : 3,
+  arrows: false,
+  infinte: true,
+  swipeToSlide: true
 });
