42b4d309effba05e1c1a7d9b492ac010041bddab
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-05-08 06:40:06 +0530

    First

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// NAVBAR
+$(document).ready(function() {
+	$('.sidenav').sidenav();
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById('navbar').style.top = '0';
+	} else {
+		document.getElementById('navbar').style.top = '-100px';
+	}
+	prevScrollpos = currentScrollPos;
+};
+
+// GALLERY
+$('.img-slide').slick({
+	lazyLoad: 'progressive',
+	autoplay: true,
+	autoplaySpeed: 2000,
+	slidesToShow: 3,
+	arrows: false,
+	infinte: true,
+	swipeToSlide: true
+});
