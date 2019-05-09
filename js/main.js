57ba321a0ee99b55b713bed1a2e1af3d00dceaf4
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-05-09 15:52:01 +0530

    Updated Carousel Options

--- js/main.js
+++ js/main.js
@@ -1,38 +1,39 @@
 // NAVBAR
 $(document).ready(function() {
-  $(".sidenav").sidenav();
+	$('.sidenav').sidenav();
 });
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById('navbar').style.top = '0';
+	} else {
+		document.getElementById('navbar').style.top = '-100px';
+	}
+	prevScrollpos = currentScrollPos;
 };
 
 //SCROLL
 $(document).ready(function() {
-  $("a.scroll").click(function(event) {
-    event.preventDefault();
-    $("html, body").animate(
-      { scrollTop: $($(this).attr("href")).offset().top },
-      500
-    );
-  });
+	$('a.scroll').click(function(event) {
+		event.preventDefault();
+		$('html, body').animate(
+			{ scrollTop: $($(this).attr('href')).offset().top },
+			500
+		);
+	});
 });
 
 // GALLERY
+let sts = window.innerWidth < 650 ? (window.innerWidth < 450 ? 1 : 2) : 3;
 console.log(window.innerWidth);
-$(".img-slide").slick({
-  lazyLoad: "progressive",
-  autoplay: true,
-  autoplaySpeed: 2000,
-  slidesToShow: window.innerWidth < 650 ? (window.innerWidth < 450 ? 1 : 2) : 3,
-  arrows: false,
-  infinte: true,
-  swipeToSlide: true
+$('.img-slide').slick({
+	lazyLoad: 'progressive',
+	autoplay: true,
+	autoplaySpeed: 2000,
+	slidesToShow: sts,
+	arrows: false,
+	infinte: true,
+	swipeToSlide: true
 });
